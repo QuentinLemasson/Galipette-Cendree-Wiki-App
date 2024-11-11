3e2073cfed2ce6aa561839d1c5f01b3f190e996a
Quentin <altakglag@gmail.com>
2024-11-11 19:10:07 +0100

    deploy: updated dbclient

--- src/utils/server/getDbClient.lib.ts
+++ src/utils/server/getDbClient.lib.ts
@@ -2,22 +2,10 @@ import { Client } from "pg";
 
 // Database connection helper
 export async function getDbClient() {
-  let client;
-
-  if (process.env.NODE_ENV === "production") {
-    client = new Client({
-      connectionString: process.env.NEON_DB_URL,
-      ssl: true,
-    });
-  } else {
-    client = new Client({
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOST,
-      database: process.env.DB_DATABASE,
-      port: parseInt(process.env.DB_PORT || "5432"),
-    });
-  }
+  const client = new Client({
+    connectionString: process.env.DB_URL,
+    ssl: true,
+  });
 
   await client.connect();
   return client;
