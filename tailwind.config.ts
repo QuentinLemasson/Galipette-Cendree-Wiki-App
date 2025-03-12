37ae298b3cc206581f45cb0942c58226bf466b21
Quentin Lemasson <altakglag@gmail.com>
2025-03-12 21:52:16 +0100

    chore: swhadcn init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,29 +3,72 @@ import type { Config } from "tailwindcss";
 import { addIconSelectors } from "@iconify/tailwind";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      skew: {
-        "30": "30deg",
-      },
-      gridTemplateColumns: {
-        // Complex site-specific column configuration
-        banner: "1fr 50% 1fr",
-      },
-      animation: {
-        sparkle: "sparkle 1s ease-out forwards",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		skew: {
+  			'30': '30deg'
+  		},
+  		gridTemplateColumns: {
+  			banner: '1fr 50% 1fr'
+  		},
+  		animation: {
+  			sparkle: 'sparkle 1s ease-out forwards'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [addIconSelectors(["mdi", "logos", "vscode-icons", "fa-solid"])],
+  plugins: [addIconSelectors(["mdi", "logos", "vscode-icons", "fa-solid"]), require("tailwindcss-animate")],
 };
 export default config;
